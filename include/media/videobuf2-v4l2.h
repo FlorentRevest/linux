c66f2f4622a495a42fe09ee7cb98a438e51af3b1
Hans Verkuil <hans.verkuil@cisco.com>
2015-04-09 13:30:35 +0200

    videodev2.h: add request field to v4l2_buffer.
    When queuing buffers allow for passing the request ID that
    should be associated with this buffer. Split the u32 reserved2 field
    into two u16 fields, one for request, one with the old reserved2 name.
    
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Florent Revest <florent.revest@free-electrons.com>

--- include/media/videobuf2-v4l2.h
+++ include/media/videobuf2-v4l2.h
@@ -29,6 +29,7 @@
  * @flags:	buffer informational flags
  * @field:	enum v4l2_field; field order of the image in the buffer
  * @timecode:	frame timecode
+ * @request:	this buffer should use this request
  * @sequence:	sequence count of this frame
  * Should contain enough information to be able to cover all the fields
  * of struct v4l2_buffer at videodev2.h
@@ -39,6 +40,7 @@ struct vb2_v4l2_buffer {
 	__u32			flags;
 	__u32			field;
 	struct v4l2_timecode	timecode;
+	__u16			request;
 	__u32			sequence;
 };
 
