67da339ed08346cf3f505134863c955d26bfb8ac
Hans Verkuil <hans.verkuil@cisco.com>
2015-04-13 15:53:34 +0200

    vb2: add helper function to queue request-specific buffer.
    The vb2_qbuf_request() function will queue any buffers for the given request
    that are in state PREPARED.
    
    Useful when drivers have to implement the req_queue callback.
    
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>

--- include/media/videobuf2-v4l2.h
+++ include/media/videobuf2-v4l2.h
@@ -57,6 +57,7 @@ int vb2_create_bufs(struct vb2_queue *q, struct v4l2_create_buffers *create);
 int vb2_prepare_buf(struct vb2_queue *q, struct v4l2_buffer *b);
 
 int vb2_qbuf(struct vb2_queue *q, struct v4l2_buffer *b);
+int vb2_qbuf_request(struct vb2_queue *q, u16 request, struct vb2_buffer **p_buf);
 int vb2_expbuf(struct vb2_queue *q, struct v4l2_exportbuffer *eb);
 int vb2_dqbuf(struct vb2_queue *q, struct v4l2_buffer *b, bool nonblocking);
 
